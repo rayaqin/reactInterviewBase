f42dc47dc53214b88d64733d191950796d1d16e8
Bálint Ujvári <rayaqin@gmail.com>
2025-03-21 17:38:25 +0100

    first skeleton

---
+++ react-typescript-app/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="flex rounded-md m-2 px-4 py-2 bg-[#251111] text-white w-fit">
+        <ul>
+          <li className="my-2">1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.</li>
+          <li className="my-2">2. A robot must obey the orders given to it by human beings, except where such orders would conflict with the First Law.</li>
+          <li className="my-2">3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
+
