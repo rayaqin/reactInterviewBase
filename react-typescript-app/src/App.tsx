fd410c498e3822639d7a0fa88aa7f6409294dc32
Bálint Ujvári <rayaqin@gmail.com>
2025-06-11 16:16:57 +0200

    remove unnecessary stuff

--- react-typescript-app/src/App.tsx
+++ react-typescript-app/src/App.tsx
@@ -50,22 +50,6 @@ function App() {
 
   return (
     <>
-      <div className="rounded-md m-2 px-4 py-2 bg-[#251111] text-white w-fit">
-        <ul>
-          <li className="my-2">
-            1. A robot may not injure a human being or, through inaction, allow a human being to
-            come to harm.
-          </li>
-          <li className="my-2">
-            2. A robot must obey the orders given to it by human beings, except where such orders
-            would conflict with the First Law.
-          </li>
-          <li className="my-2">
-            3. A robot must protect its own existence as long as such protection does not conflict
-            with the First or Second Law.
-          </li>
-        </ul>
-      </div>
       <div className="border-2 border-blue-500">stuff above</div>
       {/* This is where we put the jsx code returned by the function, we tell the browser where to show it in the DOM */}
       {AnimatedTestimonialsDemo()}
